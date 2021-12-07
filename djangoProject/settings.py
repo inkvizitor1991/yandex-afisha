d2ca4f1fd0f59b2309c32ee570bea630353084db
inkvizitor1991 <85788805+inkvizitor1991@users.noreply.github.com>
2021-12-07 12:36:54 +0300

    Setup media files serving

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -86,7 +86,9 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from djangoProject import views
@@ -6,3 +8,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.yandex_afisha),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
