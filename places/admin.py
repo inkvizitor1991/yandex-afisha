410757f04df365f1a5883b23aef204e75738fc78
inkvizitor1991 <85788805+inkvizitor1991@users.noreply.github.com>
2021-12-11 18:06:16 +0300

    Show image preview in Django admin

--- places/admin.py
+++ places/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
 from .models import Places, Images
 
+from django.utils.html import format_html
+
 
 class ImageInline(admin.TabularInline):
     model = Images
+    fields = [
+        'title', 'description_short',
+        'description_long', 'lat',
+        'lon', 'show_image'
+    ]
+    readonly_fields = ['show_image']
+
+    def show_image(self, instance):
+        return format_html(
+            f'<img src="{instance.image.url}" style="max-height: 200px;">')
+
+    show_image.short_description = "Image"
 
 
 @admin.register(Places)
