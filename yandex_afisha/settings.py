558d9975d9f9555c16c15af2b88ffc40c93ab8b2
inkvizitor1991 <85788805+inkvizitor1991@users.noreply.github.com>
2021-12-29 15:30:03 +0300

    fixed the DEBUG settings

--- yandex_afisha/settings.py
+++ yandex_afisha/settings.py
@@ -1,14 +1,19 @@
 import os
 
 from pathlib import Path
+from dotenv import load_dotenv
+
+
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
+ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
